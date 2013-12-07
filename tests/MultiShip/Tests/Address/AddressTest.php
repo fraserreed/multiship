7d85beeb0252703d51fe75bf65a9560b6221b8c3
Fraser Reed <fraser@canvaspop.com>
2013-12-07 16:37:31 -0500

    added licensing information

--- tests/MultiShip/Tests/Address/AddressTest.php
+++ tests/MultiShip/Tests/Address/AddressTest.php
@@ -1,4 +1,15 @@
 <?php
+
+/*
+ * This file is part of the MultiShip package.
+ *
+ * (c) 2013 Fraser Reed
+ *      <fraser.reed@gmail.com>
+ *      github.com/fraserreed
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace MultiShip\Tests\Address;
 
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,26 @@
 <?php
 
-error_reporting(E_ALL | E_STRICT);
+/*
+ * This file is part of the MultiShip package.
+ *
+ * (c) 2013 Fraser Reed
+ *      <fraser.reed@gmail.com>
+ *      github.com/fraserreed
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+error_reporting( E_ALL | E_STRICT );
 
 // Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
-    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
-        . "See http://getcomposer.org for help with installing composer\n");
+if( !file_exists( dirname( __DIR__ ) . '/composer.lock' ) )
+{
+    die( "Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
+        . "See http://getcomposer.org for help with installing composer\n" );
 }
 
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 // Include the composer autoloader
-$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+$autoloader = require dirname( __DIR__ ) . '/vendor/autoload.php';
